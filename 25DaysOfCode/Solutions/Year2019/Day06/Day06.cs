4ee86a7aea4509f036fa8a2778acf75727f7e7fb
the1ullneversee <tkk100@msn.com>
2019-12-09 22:35:23 +0000

    Changes made to Day06 solution to get a correct solution.

--- 25DaysOfCode/Solutions/Year2019/Day06/Day06.cs
+++ 25DaysOfCode/Solutions/Year2019/Day06/Day06.cs
@@ -9,45 +9,93 @@ namespace _25DaysOfCode.Solutions.Year2019.Day06
     public class Day06 : AVC
     {
         string[] input;
-
+        Dictionary<string, List<string>> orbitMaps;
         public Day06() : base ("Universal Orbit Map",2019,6)
         {
             input = Input;
+            orbitMaps = new Dictionary<string, List<string>>();
+            PopulateOrbitMaps(input);
+        }
+
+        private void PopulateOrbitMaps(string[] input)
+        {
+            //AA)BB
+            foreach(var map in input)
+            {
+                var planet = map.Split(')')[0];
+                var orbiter = map.Split(')')[1];
+
+                if(orbitMaps.TryGetValue(planet, out List<string> orbits))
+                {
+                    orbits.Add(orbiter);
+                } else
+                {
+                    orbitMaps.Add(planet, new List<string> { orbiter });
+                }
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return CalculateSharedOrbits(input).ToString();
+            return CalculateSharedOrbits(orbitMaps).ToString();
+            //return CalculateSharedOrbits(input).ToString();
+            return "";
         }
 
         protected override string SolvePartTwo()
         {
-            throw new NotImplementedException();
+            return "";
         }
 
-        public int CalculateSharedOrbits(string[] orbitMap)
+        public int CalculateSharedOrbits(Dictionary<string, List<string>> orbitMaps)
         {
-
-            HashSet<string> uo = new HashSet<string>();
-
-            foreach(var pair in orbitMap)
+            //foreach orbit map
+            //foreach orbiter in the map
+            //search for paths to that orbiter.
+            int sum = 0;
+            foreach(var parent in orbitMaps.Keys)
             {
-                var parent = pair.Split(')')[0];
-                int length = parent.Length;
-                var child = pair.Split(')')[1];
-                var matches = uo.Where(x => x.Substring(x.Length - length) == parent);
-                if(matches.Count() == 0)
+                foreach(var orbiter in orbitMaps[parent])
                 {
-                    uo.Add($"{parent}{child}");
+                    sum++;
+                    sum += FindOrbits(orbiter);
                 }
-                else
+            }
+
+            return sum;
+        }
+
+        public int FindOrbits(string planet)
+        {
+            int sum = 0;
+            if (orbitMaps.TryGetValue(planet, out List<string> orbiters))
+            {
+                foreach (var orbiter in orbiters)
                 {
-                    var found = true;
+                    sum++;
+                    sum += FindOrbits(orbiter);
                 }
+            }
+            return sum;
+        }
+
+        public List<string> FindPathToPlanet(Dictionary<string, List<string>> orbitMap, string planet)
+        {
+            //var paths = orbitMap.Values(x => x.Contains(planet));
+            string me = "YOU";
+            string santa = "SANTA";
+
+            foreach (var reference in orbitMap.Keys)
+            {
                 
             }
 
-            return 0;
+            if(orbitMap.TryGetValue(planet, out List<string> orbiters))
+            {
+
+            }
+
+            return new List<string>();
         }
     }
 }
